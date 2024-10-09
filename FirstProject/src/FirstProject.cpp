5cfd7f5b629f3c8bc7f8615eb537b1e592d7bd87
Taras <taras.neridnyi.ri.2024@lpnu.ua>
2024-10-10 01:15:28 +0300

    Update FirstProject.cpp 5

--- FirstProject/src/FirstProject.cpp
+++ FirstProject/src/FirstProject.cpp
@@ -30,5 +30,10 @@ int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
 	return 0;
 }
